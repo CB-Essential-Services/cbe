b049f31a4de948114517efb2a8b4213b287f292f
MCB-11561 <m.boyle.91x@gmail.com>
2021-01-03 19:39:14 -0500

    Changes

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,20 +4,14 @@ set -e
 set -o pipefail
 set -v
 
-echo "stackbit-build.sh: start build"
-
-# get the first commit hash and run studio-build.js, it will install and deploy sanity studio only if needed
-# to optimize the build time, studio-build.js runs in background in parallel to site build command
 initialGitHash=$(git rev-list --max-parents=0 HEAD)
 node ./studio-build.js $initialGitHash &
 
-# fetch data from CMS through stackbit-pull
+curl -s -X POST https://api.stackbit.com/project/5fdfc368f50fa000165eaccb/webhook/build/pull > /dev/null
 npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5fdfc368f50fa000165eaccb
-
-# build site
-npm run build
-
-# wait for studio-build.js
+curl -s -X POST https://api.stackbit.com/project/5fdfc368f50fa000165eaccb/webhook/build/ssgbuild > /dev/null
+gatsby build
 wait
 
-echo "stackbit-build.sh: finished build"
+curl -s -X POST https://api.stackbit.com/project/5fdfc368f50fa000165eaccb/webhook/build/publish > /dev/null
+echo "Stackbit-build.sh finished build"
